e0219b3d02d570eebb4ae462c29c222823659130
Kai Nizhner <103528458+KONNman1@users.noreply.github.com>
2022-05-09 20:49:16 +0000

    Started part 1, finished parts 2 and 3.

--- part3.py
+++ part3.py
@@ -6,17 +6,38 @@ Write a program that asks the user to input one integer. The program will then p
 
 3 examples of what should appear on the console when the program runs (note: the test driver is case sensitive):
 
-Enter a number:  -2
-negative
-not divisible by 3
+Enter a number: -2
+Negative
+Mot divisible by 3
 
-Enter a number:  0
-zero
-divisible by 3
+Enter a number: 0
+Zero
+Divisible by 3
 
 Enter a number:  5
-positive
-not divisible by 3
+Positive
+Not divisible by 3
 '''
 
+
 #write your code below
+number = int(input("Enter an integer: "))
+divisible = number % 3
+
+if number > 0:
+  print ("Positive")
+  if divisible == 0:
+    print("Divisible by 3")
+  if divisible != 0:
+    print("Not divisible by 3")
+
+if number < 0:
+  print ("Negative")
+  if divisible == 0:
+    print("Divisible by 3")
+  if divisible != 0:
+    print("Not divisible by 3")
+
+if number == 0:
+  print("Zero")
+  print("Divisible by 3")
